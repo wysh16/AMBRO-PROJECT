d9dd501d619eb41ec0ff9662110729c3227d6100
Nhi <nhindp22406@st.uel.edu.vn>
2024-11-21 09:24:23 +0700

    Cập nhật trang công thức ( thanh tìm kiếm)

--- FRONTEND/src/app/components/congthuc/congthuc.component.ts
+++ FRONTEND/src/app/components/congthuc/congthuc.component.ts
@@ -5,17 +5,28 @@ import { CongThuc } from '../../types/congthuc';
 import { CommonModule } from '@angular/common';
 
 import { Router, RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-congthuc',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './congthuc.component.html',
   styleUrl: './congthuc.component.css',
 })
 export class CongthucComponent implements OnInit {
   CongThucService = inject(CongthucService);
   CongThucList: CongThuc[] = [];
+  filteredCongThucList: CongThuc[] = [];
   selectedCongThuc: CongThuc | null = null;
+  searchQuery: string = '';
 
   // constructor(private sanitizer: DomSanitizer) {}
   constructor(private _service: CongthucService, private router: Router) {}
@@ -24,9 +35,25 @@ export class CongthucComponent implements OnInit {
     this.CongThucService.getAllCongThuc().subscribe((result) => {
       this.CongThucList = result;
     });
+    this.filteredCongThucList = [...this.CongThucList];
+  }
+
+  onSearch() {
+    const query = this.searchQuery.trim().toLowerCase();
+    if (query) {
+      this.filteredCongThucList = this.CongThucList.filter((congthuc) =>
+        congthuc.name.toLowerCase().includes(query)
+      );
+    } else {
+      this.filteredCongThucList = [...this.CongThucList];
+    }
   }
 
   selectCongThuc(congthuc: CongThuc) {
     this.selectedCongThuc = congthuc;
+    this.searchQuery = congthuc.name ? String(congthuc.name) : '';
+    this.filteredCongThucList = [];
+    // Điều hướng đến chi tiết công thức
+    this.router.navigate(['/congthuc', congthuc._id]);
   }
 }
